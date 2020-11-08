744a1ef6aeda969e81bf56c8d06fc9d1b664d5e9
Pun Kongsomboon <ksm_punn@hotmail.com>
2020-11-08 18:08:12 +0700

    Editplace html complete

--- public/script/Editplace.js
+++ public/script/Editplace.js
@@ -1,4 +1,33 @@
 $(document).ready(function () {
-    $("#sortable").sortable();
-    $("#sortable").disableSelection();
+    // $(".sortable").sortable();
+    // $(".sortable").disableSelection();
+
+    $("#sortable1, #sortable2").sortable({
+        connectWith: ".connectedSortable"
+    }).disableSelection();
+
+    $("#test").click(function () {
+        var itemOrder = $('#sortable1').sortable("toArray");
+        for (var i = 0; i < itemOrder.length; i++) {
+            console.log("Position: " + i + " ID: " + itemOrder[i]);
+        }
+    });
+
+    $("#sortable1").sortable({
+        update: function () {
+            var order = $(this).sortable('toArray').toString();
+            
+            
+        }
+    });
+
+    $("#btnSave").click(function(){
+        window.history.back();
+    });
+
+    $("#btnback").click(function(){
+        window.history.back();
+    });
+
+
 });
