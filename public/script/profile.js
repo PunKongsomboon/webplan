5115a74e53e146ca0e0e3319dfec41bedd476c59
Pun Kongsomboon <ksm_punn@hotmail.com>
2020-11-08 21:31:12 +0700

    Profile page complete

---
+++ public/script/profile.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".btn-outline-secondary").click(function(){
+        $("#modelDetailplan").modal("show");
+    });
+
+    $("#btnChange-password").click(function(){
+        alert("test");
+    });
+    
+});
